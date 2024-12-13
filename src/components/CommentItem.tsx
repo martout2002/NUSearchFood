b257ad67f57520a7f8bcadab462394b1ada6bdac
Martin Ng <martinng2002@gmail.com>
2024-12-13 23:51:23 +1100

    Add new App bar and thread component styling

--- src/components/CommentItem.tsx
+++ src/components/CommentItem.tsx
@@ -3,6 +3,11 @@ import Comment from "../types/Comment";
 import React from "react";
 import { Card, CardContent, Typography } from "@mui/material";
 import { makeStyles } from "@mui/styles";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import Divider from "@mui/material/Divider";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import CommentIcon from "@mui/icons-material/Comment";
 
 type Props = {
     comment: Comment;
@@ -10,7 +15,7 @@ type Props = {
 };
 const useStyles = makeStyles(() => ({
     commentBody: {
-        fontSize: 16,
+        fontSize: 20,
         whiteSpace: "pre-wrap",
         paddingBottom: "1em",
     },
@@ -27,14 +32,21 @@ const CommentItem: React.FC<Props> = ({ comment, styled }) => {
 
     if (styled) {
         return (
-            <Card className={classes.commentCard}>
+            <Card className={classes.commentCard} sx={{ textAlign: "left" }}>
                 <CardContent>
-                    <Typography variant="body2" color="textPrimary" className={classes.commentBody} component="p">
-                        {comment.body}
-                    </Typography>
-                    <Typography color="textSecondary" className={classes.metadata} gutterBottom>
-                        {"Posted by " + comment.author + " on " + comment.timestamp.toLocaleString()}
-                    </Typography>
+                    <Stack spacing={2}>
+                        <Typography variant="body2" color="textPrimary" className={classes.commentBody} component="p">
+                            {comment.body}
+                        </Typography>
+                        <Typography color="textSecondary" className={classes.metadata} gutterBottom>
+                            {"Posted by " + comment.author + " on " + comment.timestamp.toLocaleString()}
+                        </Typography>
+                        <Box display="flex" alignItems="center" gap={1}>
+                            <FavoriteBorderIcon />
+                            <Divider />
+                            <CommentIcon />
+                        </Box>
+                    </Stack>
                 </CardContent>
             </Card>
         );
